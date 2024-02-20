e851855491a7441cf77a10be56d58f517648673b
marti <martiespinosa99@gmail.com>
2024-02-20 23:31:32 +0100

    Initial Commit

---
+++ Bootcamp/BootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootcampApp.swift
+//  Bootcamp
+//
+//  Created by Martí Espinosa Farran on 20/2/24.
+//
+
+import SwiftUI
+
+@main
+struct BootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bootcamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bootcamp
+//
+//  Created by Martí Espinosa Farran on 20/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
