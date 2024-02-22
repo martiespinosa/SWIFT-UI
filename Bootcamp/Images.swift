56b1f162c7268b1f86c870e68b090dc56d9623f1
marti <martiespinosa99@gmail.com>
2024-02-22 21:22:57 +0100

    images

---
+++ Bootcamp/Images.swift
@@ -0,0 +1,29 @@
+//
+//  Images.swift
+//  Bootcamp
+//
+//  Created by Martí Espinosa Farran on 22/2/24.
+//
+
+import SwiftUI
+
+struct Images: View {
+    var body: some View {
+        Image("elon")
+            .resizable()
+            .scaledToFill()
+            .frame(width: 300, height: 300)
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+        
+        Image("tesla")
+            .renderingMode(/*@START_MENU_TOKEN@*/.template/*@END_MENU_TOKEN@*/) // this can also be putted on Assets and that way this image will be a template by default so sw can colorate the image with foregroundColor
+            .resizable()
+            .scaledToFit()
+            .frame(width: 300, height: 300)
+            .foregroundColor(.green)
+    }
+}
+
+#Preview {
+    Images()
+}
