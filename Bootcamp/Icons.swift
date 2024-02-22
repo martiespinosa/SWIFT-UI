2999113fbab495f34c3d479228be4ea52b70ef48
marti <martiespinosa99@gmail.com>
2024-02-22 18:33:47 +0100

    icons

--- Bootcamp/Icons.swift
+++ Bootcamp/Icons.swift
@@ -10,12 +10,27 @@ import SwiftUI
 struct Icons: View {
     var body: some View {
         Image(systemName: "heart.fill")
+            .resizable()
+            //.scaledToFill()
+            .scaledToFit()
             .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
             .foregroundColor(.green)
+            .frame(width: 100, height: 100)
+            //.clipped()
         
         Image(systemName: "star.fill")
+            .resizable()
+            .scaledToFit()
             .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
             .foregroundColor(.blue)
+            .frame(width: 100, height: 100)
+        
+        Image(systemName: "battery.100.bolt")
+            .resizable()
+            .renderingMode(.original)
+            //.foregroundStyle(.orange, .gray, .purple)
+            .aspectRatio(contentMode: .fit)
+            .frame(width: 150, height: 150)
     }
 }
 
