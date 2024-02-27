29ee77d43cf644c25a314203cb3a72764519bf26
marti <martiespinosa99@gmail.com>
2024-02-27 22:22:20 +0100

    forEachs

---
+++ Bootcamp/ForEachs.swift
@@ -0,0 +1,29 @@
+//
+//  ForEachs.swift
+//  Bootcamp
+//
+//  Created by Martí Espinosa Farran on 27/2/24.
+//
+
+import SwiftUI
+
+struct ForEachs: View {
+    
+    let fruits: [String] = ["apple", "orange", "banana"]
+    
+    var body: some View {
+        VStack {
+//            ForEach(0..<10) { index in
+//                Text("\(index): Hello, World!")
+//            }
+            
+            ForEach(fruits.indices) { index in
+                Text("\(fruits[index])")
+            }
+        }
+    }
+}
+
+#Preview {
+    ForEachs()
+}
