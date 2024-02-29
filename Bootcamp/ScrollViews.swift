fbdcf20d6a1ec9c4f8b592262785cb59d3fff72a
marti <martiespinosa99@gmail.com>
2024-02-29 16:11:49 +0100

    Scrollview

--- Bootcamp/ScrollViews.swift
+++ Bootcamp/ScrollViews.swift
@@ -9,15 +9,15 @@ import SwiftUI
 
 struct ScrollViews: View {
     
-    let colors: [Color] = [.blue, .green, .yellow, .orange, .brown]
+    let colors: [Color] = [.blue, .green, .yellow, .orange, .brown, .purple, .pink, .teal, .mint, .cyan]
     
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
             LazyVStack {
-                ForEach(0..<25) { index in
+                ForEach(0..<30) { index in
                     ScrollView(.horizontal, showsIndicators: false) {
                         LazyHStack {
-                            ForEach(0..<5) { index in
+                            ForEach(0..<10) { index in
                                 RoundedRectangle(cornerRadius: 25.0)
                                     .fill(colors[index])
                                     .frame(width: 150, height: 150)
