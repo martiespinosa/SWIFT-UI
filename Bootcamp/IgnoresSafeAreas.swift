654eba2693b9699fa97ae2d79f564c5765dbdf39
marti <martiespinosa99@gmail.com>
2024-02-29 19:43:21 +0100

    IgnoresSafeAreas

--- Bootcamp/IgnoresSafeAreas.swift
+++ Bootcamp/IgnoresSafeAreas.swift
@@ -9,25 +9,27 @@ import SwiftUI
 
 struct IgnoresSafeAreas: View {
     var body: some View {
-        ScrollView {
-            VStack() {
-                Text("Hello, World!")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity, alignment: .top)
-                
-                ForEach(0..<10) { index in
-                    RoundedRectangle(cornerRadius: 25.0)
-                        .fill(.black.opacity(0.6))
-                        .frame(height: 150)
-                }
-            }
-            .padding()
-        }
-        .background(
+        ZStack {
             Color.brown
-                //.ignoresSafeArea() // it seems like now backgrounds igores the safe area by default...
-        )
+                .ignoresSafeArea()
+            
+            ScrollView {
+                VStack() {
+                    Text("Hello, World!")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity, alignment: .top)
+                    
+                    ForEach(0..<10) { index in
+                        RoundedRectangle(cornerRadius: 25.0)
+                            .fill(.black.opacity(0.6))
+                            .frame(height: 150)
+                    }
+                }
+                .padding()
+            }
+        }
+ 
     }
 }
 
