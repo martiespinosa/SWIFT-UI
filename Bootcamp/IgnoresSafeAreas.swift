69f16fad29a601bd7ff78490070f06f857a09a7b
marti <martiespinosa99@gmail.com>
2024-02-29 16:46:54 +0100

    IgnoresSafeAreas

---
+++ Bootcamp/IgnoresSafeAreas.swift
@@ -0,0 +1,36 @@
+//
+//  IgnoreSafeAreas.swift
+//  Bootcamp
+//
+//  Created by Martí Espinosa Farran on 29/2/24.
+//
+
+import SwiftUI
+
+struct IgnoresSafeAreas: View {
+    var body: some View {
+        ScrollView {
+            VStack() {
+                Text("Hello, World!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .frame(maxWidth: .infinity, alignment: .top)
+                
+                ForEach(0..<10) { index in
+                    RoundedRectangle(cornerRadius: 25.0)
+                        .fill(.black.opacity(0.6))
+                        .frame(height: 150)
+                }
+            }
+            .padding()
+        }
+        .background(
+            Color.brown
+                //.ignoresSafeArea() // here it is not necessary i don't know why
+        )
+    }
+}
+
+#Preview {
+    IgnoresSafeAreas()
+}
