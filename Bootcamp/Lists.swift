2456c3215542969d2b2147b07aed847ea5d92bf5
marti <martiespinosa99@gmail.com>
2024-03-09 11:59:51 +0100

    lists

--- Bootcamp/Lists.swift
+++ Bootcamp/Lists.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct Lists: View {
+    
+    @State var fruits: [String] = ["Apple", "Peach", "Grape"]
+    @State var vegetables: [String] = ["Carrot", "Broccoli", "Potato"]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Section("Fruits") {
+                    ForEach(fruits, id: \.self) { fruit in
+                        Text(fruit)
+                    }
+                    .onDelete(perform: delete)
+                    .onMove(perform: move)
+                }
+                
+                Section("Vegetables") {
+                    ForEach(vegetables, id: \.self) { vegetable in
+                        Text(vegetable)
+                    }
+                }
+                
+            }
+            .listStyle(.sidebar)
+            .navigationTitle("Grocery List")
+            .navigationBarItems(
+                leading: EditButton(),
+                trailing: Button("Add") {
+                    add()
+                }
+            )
+        }
+    }
+    
+    func delete(indexSet: IndexSet) {
+        fruits.remove(atOffsets: indexSet)
+    }
+    
+    func move(indices: IndexSet, newOffset: Int) {
+        fruits.move(fromOffsets: indices, toOffset: newOffset)
+    }
+    
+    func add() {
+        fruits.append("Coconut")
     }
 }
 
