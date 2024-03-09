6f0c091ce166b2c90b040b6b7635b75ee0da4f3d
marti <martiespinosa99@gmail.com>
2024-03-09 11:59:08 +0100

    lists

---
+++ Bootcamp/Lists.swift
@@ -0,0 +1,18 @@
+//
+//  Lists.swift
+//  Bootcamp
+//
+//  Created by Martí Espinosa Farran on 9/3/24.
+//
+
+import SwiftUI
+
+struct Lists: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Lists()
+}
