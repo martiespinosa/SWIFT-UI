cf23cc2a18e15b3186414ab9a1b2fd0fdf20392e
marti <martiespinosa99@gmail.com>
2024-03-15 00:22:19 +0100

    if let & guard let

---
+++ Bootcamp/IfLetGuardLet.swift
@@ -0,0 +1,62 @@
+//
+//  IfLetGuardLet.swift
+//  Bootcamp
+//
+//  Created by Martí Espinosa Farran on 15/3/24.
+//
+
+import SwiftUI
+
+struct IfLetGuardLet: View {
+    
+    @State var displayText: String? = nil
+    @State var userID: Int? = 22
+    @State var isLoading: Bool = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                if let text = displayText {
+                    Text(text)
+                        .font(.title)
+                }
+                
+                if isLoading {
+                    ProgressView()
+                }
+            }
+            .navigationTitle("Safe coding")
+            .onAppear() {
+                loadData2()
+            }
+        }
+    }
+    
+    func loadData() {
+        if let userID = userID {
+            isLoading = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                displayText = "This is your ID: \(userID)"
+                isLoading = false
+            }
+        } else {
+            displayText = "Error:\nYou don't have user ID"
+        }
+    }
+    
+    func loadData2() {
+        guard let userID = userID else {
+            displayText = "Error:\nYou don't have user ID"
+            return
+        }
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            displayText = "This is your ID: \(userID)"
+            isLoading = false
+        }
+    }
+}
+
+#Preview {
+    IfLetGuardLet()
+}
