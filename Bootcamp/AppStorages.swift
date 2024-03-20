1abd921be013e185acd7630a794f60b407365a96
marti <martiespinosa99@gmail.com>
2024-03-20 23:44:46 +0100

    text field added

--- Bootcamp/AppStorages.swift
+++ Bootcamp/AppStorages.swift
@@ -10,13 +10,17 @@ import SwiftUI
 struct AppStorages: View {
     
     @AppStorage("name") var currentUserName: String? // We use @AppStorage to persist small pices of data
+    @State var textFieldText: String = ""
     
     var body: some View {
         VStack(spacing: 20) {
             Text(currentUserName ?? "Add name here")
+                .font(.title)
+            TextField("Your name is...", text: $textFieldText)
+                .padding(.horizontal, 50)
             
             Button("Save".uppercased()) {
-                currentUserName = "Marti"
+                currentUserName = textFieldText
             }
         }
     }
