6f5425ca936b4a27acf6b84ce735a9452762a952
marti <martiespinosa99@gmail.com>
2024-03-23 23:27:31 +0100

    text selections

---
+++ Bootcamp/TextSelections.swift
@@ -0,0 +1,19 @@
+//
+//  TextSelection.swift
+//  Bootcamp
+//
+//  Created by Martí Espinosa Farran on 23/3/24.
+//
+
+import SwiftUI
+
+struct TextSelections: View {
+    var body: some View {
+        Text("Hello, World!")
+            .textSelection(.enabled)
+    }
+}
+
+#Preview {
+    TextSelections()
+}
